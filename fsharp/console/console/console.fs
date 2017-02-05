0f1a252905440f2eacc931662eb3a6a3fc3513e1
Alex Falkowski <alexrfalkowski@gmail.com>
2017-02-05 20:49:52 +0800

    My first fsharp program.

--- fsharp/console/console/console.fs
+++ fsharp/console/console/console.fs
@@ -1,6 +1,22 @@
 module Console
 
+let splitAtSpaces(name: string) =
+    name.Split ' '
+    |> Array.toList
+
+let wordCount text =
+    let words = splitAtSpaces text
+    let numWords = words.Length
+    let distinctWords = List.distinct words
+    let numDups = numWords - distinctWords.Length
+    (numWords, numDups)
+
+let showWordCount text =
+    let numWords, numDups = wordCount text
+    printfn "%d words in text" numWords
+    printfn "%d duplicate words" numDups
+
 [<EntryPoint>]
 let main argv =
-    printfn "%A" argv
+    showWordCount "I love you you"
     0 // return an integer exit code
