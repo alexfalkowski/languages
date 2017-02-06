f80d60e8ce75abd2e79a34caa37ac03896d1d6be
Alex Falkowski <alexrfalkowski@gmail.com>
2017-02-06 16:12:11 +0800

    Added HtmlProvider.

--- fsharp/console/console/console.fs
+++ fsharp/console/console/console.fs
@@ -1,6 +1,8 @@
 open FSharp.Data
 
 module Console =
+    type Species = HtmlProvider<"http://en.wikipedia.org/wiki/The_world's_100_most_threatened_species">
+
     let splitAtSpaces(name: string) =
         name.Split ' '
         |> Array.toList
@@ -17,7 +19,12 @@ module Console =
         printfn "%d words in text" numWords
         printfn "%d duplicate words" numDups
 
+    let printSpecies =
+        let species = [ for x in Species.GetSample().Tables.``Species list``.Rows ->  x.Type, x.``Common name`` ]
+        printfn "%A" species
+
 [<EntryPoint>]
 let main argv =
     Console.showWordCount "I love you you"
-    0 // return an integer exit code
+    Console.printSpecies
+    0
