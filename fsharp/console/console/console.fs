406bb071b7564f48840244fa24d48ada73639186
Alex Falkowski <alexrfalkowski@gmail.com>
2017-02-06 15:27:03 +0800

    Put in a module.

--- fsharp/console/console/console.fs
+++ fsharp/console/console/console.fs
@@ -1,22 +1,23 @@
-module Console
+open FSharp.Data
 
-let splitAtSpaces(name: string) =
-    name.Split ' '
-    |> Array.toList
+module Console =
+    let splitAtSpaces(name: string) =
+        name.Split ' '
+        |> Array.toList
 
-let wordCount text =
-    let words = splitAtSpaces text
-    let numWords = words.Length
-    let distinctWords = List.distinct words
-    let numDups = numWords - distinctWords.Length
-    (numWords, numDups)
+    let wordCount text =
+        let words = splitAtSpaces text
+        let numWords = words.Length
+        let distinctWords = List.distinct words
+        let numDups = numWords - distinctWords.Length
+        (numWords, numDups)
 
-let showWordCount text =
-    let numWords, numDups = wordCount text
-    printfn "%d words in text" numWords
-    printfn "%d duplicate words" numDups
+    let showWordCount text =
+        let numWords, numDups = wordCount text
+        printfn "%d words in text" numWords
+        printfn "%d duplicate words" numDups
 
 [<EntryPoint>]
 let main argv =
-    showWordCount "I love you you"
+    Console.showWordCount "I love you you"
     0 // return an integer exit code
