9f3fad0954300e90c6002852fad489b1df4f7475
Alex Falkowski <alexrfalkowski@gmail.com>
2017-03-14 08:51:23 +0000

    Add some play code.

--- clojurescript/playground/src/playground/core.cljs
+++ clojurescript/playground/src/playground/core.cljs
@@ -6,4 +6,15 @@
 
 (enable-console-print!)
 
-(println "Hello world!")
+(defn greeting
+  ([] (greeting "Hello" "world"))
+  ([name] (greeting "Hello" name))
+  ([salutation name] (str salutation ", " name "!")))
+
+(defn average
+  ([x] x)
+  ([x y] (/ (+ x y) 2))
+  ([x y & extra] (/ (reduce + (+ x y) extra)(+ 2 (count extra)))))
+
+(println (greeting "Mr" "Alex"))
+(println (average 1 2 3 4 5))
