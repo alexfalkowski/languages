c8ee7f949825f90034556ae41459d6027fe4f92b
Alex Falkowski <alexrfalkowski@gmail.com>
2017-03-15 16:13:55 +0000

    Added put file.

--- erlang/playground/src/afile_server.erl
+++ erlang/playground/src/afile_server.erl
@@ -5,6 +5,8 @@ start(Dir) -> spawn(afile_server, loop, [Dir]).
 loop(Dir) ->
   receive
     {Client, list_dir} -> Client ! {self(), file:list_dir(Dir)};
-    {Client, {get_file, File}} -> Client ! {self(), file:read_file(filename:join(Dir, File))}
+    {Client, {get_file, File}} -> Client ! {self(), file:read_file(filename:join(Dir, File))};
+    {Client, {put_file, File, Data}} ->
+      Client ! {self(), file:write_file(filename:join(Dir, File), io_lib:fwrite("~p\n", [Data]))}
   end,
   loop(Dir).
