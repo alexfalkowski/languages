3f47372441fa98bdfdad670a34334c742e1956c3
Alex Falkowski <alexrfalkowski@gmail.com>
2017-03-16 12:34:28 +0000

    Use list comprehensions.

--- erlang/playground/src/shop.erl
+++ erlang/playground/src/shop.erl
@@ -1,8 +1,8 @@
 -module(shop).
 -export([total/1]).
--import(lists, [map/2, sum/1]).
+-import(lists, [sum/1]).
 
-total(L) -> sum(map(fun({What, N}) -> cost(What) * N end, L)).
+total(L) -> sum([cost(A) *  B || {A, B} <- L]).
 
 cost(oranges) -> 5;
 cost(newspaper) -> 8;
