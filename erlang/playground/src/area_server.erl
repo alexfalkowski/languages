fa973c16087df689640cdde6c3331ed54e1f8057
Alex Falkowski <alexrfalkowski@gmail.com>
2017-03-20 19:57:37 +0000

    Make sure we listen to the correct Pid.

--- erlang/playground/src/area_server.erl
+++ erlang/playground/src/area_server.erl
@@ -1,16 +1,20 @@
 -module(area_server).
--export([rpc/2, loop/0]).
+-export([start/0, area/2, loop/0]).
+
+start() -> spawn(area_server, loop, []).
+
+area(Pid, What) -> rpc(Pid, What).
 
 rpc(Pid, Request) ->
   Pid ! {self(), Request},
   receive
-    Response -> Response
+    {Pid, Response} -> Response
   end.
 
 loop() ->
   receive
-    {From, {rectangle, Width, Ht}} -> From ! {ok, Width * Ht};
-    {From, {circle, R}} -> From! {ok, 3.14159*R*R};
-    {From, Other} -> From ! {error,Other}
+    {From, {rectangle, Width, Ht}} -> From ! {self(), {ok, Width * Ht}};
+    {From, {circle, R}} -> From! {self(), {ok, 3.14159*R*R}};
+    {From, Other} -> From ! {self(), {error,Other}}
   end,
   loop().
