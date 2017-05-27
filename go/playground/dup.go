0a25bca55c9f0e15d8d2d432f187563bb630ec58
Alex Falkowski <alexrfalkowski@gmail.com>
2017-05-27 18:36:56 -0400

    Variation of dup.

--- go/playground/dup.go
+++ go/playground/dup.go
@@ -1,27 +1,22 @@
-// Dup1 prints the text of each line that appears more than
-// once in the standard input, preceded by its count.
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
 	counts := make(map[string]int)
-	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
 		}
 	}
 	for line, n := range counts {
@@ -29,12 +24,4 @@ func main() {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
-}
-
-func countLines(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
-	// NOTE: ignoring potential errors from input.Err()
 }
