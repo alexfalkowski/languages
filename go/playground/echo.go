6a58ddec84d505f60a2917059f8dc949e9adae78
Alex Falkowski <alexrfalkowski@gmail.com>
2017-05-28 09:15:12 -0400

    Variation of go.

--- go/playground/echo.go
+++ go/playground/echo.go
@@ -2,11 +2,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"strings"
 )
 
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
 func main() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
 }
