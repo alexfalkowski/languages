c711c3298a872bc6c5349c24980d488ba464fe8e
Alex Falkowski <alexrfalkowski@gmail.com>
2017-06-02 17:23:58 -0400

    Break up tempconv module.

--- go/playground/tempconv/tempconv.go
+++ go/playground/tempconv/tempconv.go
@@ -8,6 +8,3 @@ const (
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
 )
-
-func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
