d9aa9193d1ca2e4ea20fdc5a7f94edb0a9b7414b
Alex Falkowski <alexrfalkowski@gmail.com>
2018-07-16 12:47:46 -0500

    Remove repetition.

--- elixir/playground/lib/drop.ex
+++ elixir/playground/lib/drop.ex
@@ -1,13 +1,17 @@
 defmodule Drop do
   def fall_velocity({:earth, distance}) when distance >= 0 do
-    :math.sqrt(2 * 9.8 * distance)
+    fall_velocity(9.8, distance)
   end
 
   def fall_velocity({:moon, distance}) when distance >= 0 do
-    :math.sqrt(2 * 1.6 * distance)
+    fall_velocity(1.6, distance)
   end
 
   def fall_velocity({:mars, distance}) when distance >= 0 do
-    :math.sqrt(2 * 3.71 * distance)
+    fall_velocity(3.71, distance)
+  end
+
+  defp fall_velocity(const, distance) do
+    :math.sqrt(2 * const * distance)
   end
 end
