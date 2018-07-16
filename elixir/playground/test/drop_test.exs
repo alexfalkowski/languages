5183ded6e0b9045103b81d20a3cc6b5f2e2c7bbe
Alex Falkowski <alexrfalkowski@gmail.com>
2018-07-16 12:40:33 -0500

    Added drop module.

---
+++ elixir/playground/test/drop_test.exs
@@ -0,0 +1,16 @@
+defmodule DropTest do
+  use ExUnit.Case
+  doctest Drop
+
+  test "the earth" do
+    assert Drop.fall_velocity({:earth, 20}) == 19.79898987322333
+  end
+
+  test "the moon" do
+    assert Drop.fall_velocity({:moon, 20}) == 8.0
+  end
+
+  test "mars" do
+    assert Drop.fall_velocity({:mars, 20}) == 12.181953866272849
+  end
+end
--- elixir/playground/test/playground_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PlaygroundTest do
-  use ExUnit.Case
-  doctest Playground
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
