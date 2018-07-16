09448c06f79c819ba29be73d40a3a60ddd47ca94
Alex Falkowski <alexrfalkowski@gmail.com>
2018-07-16 12:52:12 -0500

    Don't use tuple.

--- elixir/playground/lib/drop.ex
+++ elixir/playground/lib/drop.ex
@@ -1,17 +1,17 @@
 defmodule Drop do
-  def fall_velocity({:earth, distance}) when distance >= 0 do
-    fall_velocity(9.8, distance)
+  def fall_velocity(:earth, distance) when distance >= 0 do
+    fall_velocity_of_planet(9.8, distance)
   end
 
-  def fall_velocity({:moon, distance}) when distance >= 0 do
-    fall_velocity(1.6, distance)
+  def fall_velocity(:moon, distance) when distance >= 0 do
+    fall_velocity_of_planet(1.6, distance)
   end
 
-  def fall_velocity({:mars, distance}) when distance >= 0 do
-    fall_velocity(3.71, distance)
+  def fall_velocity(:mars, distance) when distance >= 0 do
+    fall_velocity_of_planet(3.71, distance)
   end
 
-  defp fall_velocity(const, distance) do
+  defp fall_velocity_of_planet(const, distance) do
     :math.sqrt(2 * const * distance)
   end
 end
--- elixir/playground/test/drop_test.exs
+++ elixir/playground/test/drop_test.exs
@@ -3,14 +3,14 @@ defmodule DropTest do
   doctest Drop
 
   test "the earth" do
-    assert Drop.fall_velocity({:earth, 20}) == 19.79898987322333
+    assert Drop.fall_velocity(:earth, 20) == 19.79898987322333
   end
 
   test "the moon" do
-    assert Drop.fall_velocity({:moon, 20}) == 8.0
+    assert Drop.fall_velocity(:moon, 20) == 8.0
   end
 
   test "mars" do
-    assert Drop.fall_velocity({:mars, 20}) == 12.181953866272849
+    assert Drop.fall_velocity(:mars, 20) == 12.181953866272849
   end
 end
