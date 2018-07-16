3c9f593138efc3400a0081f6b783c3979fec4426
Alex Falkowski <alexrfalkowski@gmail.com>
2018-07-16 16:36:39 -0500

    Format code.

--- elixir/playground/lib/bounce.ex
+++ elixir/playground/lib/bounce.ex
@@ -1,8 +1,9 @@
 defmodule Bounce do
   def report(count) do
     receive do
-      mmessage -> IO.puts("Received message:'#{mmessage}' with count:#{count}")
-      report(count + 1)
+      mmessage ->
+        IO.puts("Received message:'#{mmessage}' with count:#{count}")
+        report(count + 1)
     end
   end
 end
--- elixir/playground/mix.exs
+++ elixir/playground/mix.exs
@@ -2,12 +2,14 @@ defmodule Playground.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :playground,
-     version: "0.1.0",
-     elixir: "~> 1.6",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :playground,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
