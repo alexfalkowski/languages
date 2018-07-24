765021ecf46f16f1ff828b348304746bf60fd36f
Alex Falkowski <alexrfalkowski@gmail.com>
2018-07-24 13:15:03 -0500

    Handle monitors.

--- elixir/kv/test/registry_test.exs
+++ elixir/kv/test/registry_test.exs
@@ -15,4 +15,11 @@ defmodule KV.RegistryTest do
     KV.Bucket.put(shopping, "milk", 1)
     assert KV.Bucket.get(shopping, "milk") == 1
   end
+
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
 end
